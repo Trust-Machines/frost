a28ab146aedf3e525997d86ff45d22b1ed432d12
Joey Yandle <xoloki@gmail.com>
2022-11-15 17:37:46 -0500

    fmt fixes

--- src/frost.rs
+++ src/frost.rs
@@ -1,22 +1,14 @@
+use num_traits::{One, Zero};
+use polynomial::Polynomial;
+use rand_core::{CryptoRng, RngCore};
 use secp256k1_math::{
-    point::{
-	G, Point,
-    },
+    point::{Point, G},
     scalar::Scalar,
 };
-use num_traits::{Zero, One};
-use polynomial::Polynomial;
-use rand_core::{
-    RngCore, CryptoRng,
-};
-use sha3::{
-    Digest, Sha3_256, 
-};
+use sha3::{Digest, Sha3_256};
 
 use crate::schnorr::ID;
-use crate::util::{
-    hash_to_scalar,
-};
+use crate::util::hash_to_scalar;
 use crate::vss::VSS;
 
 #[allow(non_snake_case)]
@@ -27,7 +19,7 @@ pub struct Share {
 
 impl Share {
     pub fn verify(&self) -> bool {
-	self.id.verify(&self.A[0])
+        self.id.verify(&self.A[0])
     }
 }
 
@@ -52,10 +44,10 @@ pub struct PublicNonce {
 
 impl PublicNonce {
     pub fn from(n: &Nonce) -> Self {
-	Self {
-	    D: &n.d * G,
-	    E: &n.e * G,
-	}
+        Self {
+            D: &n.d * G,
+            E: &n.e * G,
+        }
     }
 }
 
@@ -69,106 +61,110 @@ pub struct Party {
 }
 
 impl Party {
-    pub fn new<RNG: RngCore+CryptoRng>(id: &Scalar, t: usize, rng: &mut RNG) -> Self {
-	Self {
-	    id: *id,
-	    f: VSS::random_poly(t - 1, rng),
-	    shares: Vec::new(),
-	    secret: Scalar::zero(),
-	    nonces: Vec::new(),
-	}
+    pub fn new<RNG: RngCore + CryptoRng>(id: &Scalar, t: usize, rng: &mut RNG) -> Self {
+        Self {
+            id: *id,
+            f: VSS::random_poly(t - 1, rng),
+            shares: Vec::new(),
+            secret: Scalar::zero(),
+            nonces: Vec::new(),
+        }
     }
 
-    pub fn share<RNG: RngCore+CryptoRng>(&self, rng: &mut RNG) -> Share {
-	Share {
-	    id: ID::new(&self.id, &self.f.data()[0], rng),
-	    A: (0..self.f.data().len()).map(|i| &self.f.data()[i] * G).collect(),
-	}
+    pub fn share<RNG: RngCore + CryptoRng>(&self, rng: &mut RNG) -> Share {
+        Share {
+            id: ID::new(&self.id, &self.f.data()[0], rng),
+            A: (0..self.f.data().len())
+                .map(|i| &self.f.data()[i] * G)
+                .collect(),
+        }
     }
 
     pub fn send(&mut self, share: Share2) {
-	self.shares.push(share);
+        self.shares.push(share);
     }
 
     pub fn compute_secret(&mut self) {
-	for share in &self.shares {
-	    self.secret += &share.f_i;
-	}
+        for share in &self.shares {
+            self.secret += &share.f_i;
+        }
     }
 
-    pub fn gen_nonces<RNG: RngCore+CryptoRng>(&mut self, rng: &mut RNG) {
-	const N: usize = 16;
-	self.nonces = (0..N).map(|_| Nonce {
-	    d: Scalar::random(rng),
-	    e: Scalar::random(rng),
-	}).collect();
+    pub fn gen_nonces<RNG: RngCore + CryptoRng>(&mut self, rng: &mut RNG) {
+        const N: usize = 16;
+        self.nonces = (0..N)
+            .map(|_| Nonce {
+                d: Scalar::random(rng),
+                e: Scalar::random(rng),
+            })
+            .collect();
     }
 
     #[allow(dead_code)]
     pub fn pub_nonces(&self) -> Vec<PublicNonce> {
-	self.nonces.iter().map(|n| PublicNonce::from(n)).collect()
+        self.nonces.iter().map(|n| PublicNonce::from(n)).collect()
     }
 
     #[allow(dead_code)]
     pub fn pub_nonce(&self) -> PublicNonce {
-	PublicNonce::from(self.nonces.last().unwrap())
+        PublicNonce::from(self.nonces.last().unwrap())
     }
 
-    pub fn get_nonce<RNG: RngCore+CryptoRng>(&mut self, rng: &mut RNG) -> PublicNonce {
-	if self.nonces.is_empty() {
-	    self.gen_nonces(rng);
-	}
-	PublicNonce::from(&self.nonces.last().unwrap())
+    pub fn get_nonce<RNG: RngCore + CryptoRng>(&mut self, rng: &mut RNG) -> PublicNonce {
+        if self.nonces.is_empty() {
+            self.gen_nonces(rng);
+        }
+        PublicNonce::from(&self.nonces.last().unwrap())
     }
 
     #[allow(dead_code)]
-    pub fn pop_nonce<RNG: RngCore+CryptoRng>(&mut self, rng: &mut RNG) -> PublicNonce {
-	if self.nonces.is_empty() {
-	    self.gen_nonces(rng);
-	}
-	PublicNonce::from(&self.nonces.pop().unwrap())
+    pub fn pop_nonce<RNG: RngCore + CryptoRng>(&mut self, rng: &mut RNG) -> PublicNonce {
+        if self.nonces.is_empty() {
+            self.gen_nonces(rng);
+        }
+        PublicNonce::from(&self.nonces.pop().unwrap())
     }
 
     #[allow(non_snake_case)]
     pub fn get_binding(&self, B: &Vec<PublicNonce>, msg: &String) -> Scalar {
-	let mut hasher = Sha3_256::new();
+        let mut hasher = Sha3_256::new();
 
-	hasher.update(self.id.as_bytes());
-	for b in B {
-	    hasher.update(b.D.compress().as_bytes());
-	    hasher.update(b.E.compress().as_bytes());
-	}
-	hasher.update(msg.as_bytes());
+        hasher.update(self.id.as_bytes());
+        for b in B {
+            hasher.update(b.D.compress().as_bytes());
+            hasher.update(b.E.compress().as_bytes());
+        }
+        hasher.update(msg.as_bytes());
 
-	hash_to_scalar(&mut hasher)
+        hash_to_scalar(&mut hasher)
     }
 
     #[allow(non_snake_case)]
     pub fn sign(&self, X: &Point, rho: &Scalar, R: &Point, msg: &String, l: &Scalar) -> Scalar {
-	let nonce = self.nonces.last().unwrap();
-	let mut z = &nonce.d + rho * &nonce.e;
-	
-	let mut hasher = Sha3_256::new();
+        let nonce = self.nonces.last().unwrap();
+        let mut z = &nonce.d + rho * &nonce.e;
 
-	hasher.update(X.compress().as_bytes());
-	hasher.update(R.compress().as_bytes());
-	hasher.update(msg.as_bytes());
+        let mut hasher = Sha3_256::new();
 
-	z += hash_to_scalar(&mut hasher) * &self.secret * l;
+        hasher.update(X.compress().as_bytes());
+        hasher.update(R.compress().as_bytes());
+        hasher.update(msg.as_bytes());
 
-	z
+        z += hash_to_scalar(&mut hasher) * &self.secret * l;
+
+        z
     }
 
     pub fn lambda(i: &Scalar, parties: &Vec<Party>) -> Scalar {
-	let mut l = Scalar::one();
-	
-	for p in parties {
-	    if i != &p.id {
-			l *= &p.id / (&p.id - i);
-		}
-	}
-	
-	l
+        let mut l = Scalar::one();
+
+        for p in parties {
+            if i != &p.id {
+                l *= &p.id / (&p.id - i);
+            }
+        }
+
+        l
     }
 }
 
@@ -180,45 +176,46 @@ pub struct Signature {
 
 impl Signature {
     #[allow(non_snake_case)]
-    pub fn new<RNG: RngCore+CryptoRng>(X: &Point, msg: &String, parties: &mut Vec<Party>, rng: &mut RNG) -> Self {
-	let mut B = Vec::new();
-	for party in parties.iter_mut() {
-	    B.push(party.get_nonce(rng));
-	}
+    pub fn new<RNG: RngCore + CryptoRng>(
+        X: &Point,
+        msg: &String,
+        parties: &mut Vec<Party>,
+        rng: &mut RNG,
+    ) -> Self {
+        let mut B = Vec::new();
+        for party in parties.iter_mut() {
+            B.push(party.get_nonce(rng));
+        }
 
-	let rho: Vec<Scalar> = parties.iter().map(|p| p.get_binding(&B, &msg)).collect();
+        let rho: Vec<Scalar> = parties.iter().map(|p| p.get_binding(&B, &msg)).collect();
 
-	let mut R = Point::zero();
-	for i in 0..B.len() {
-	    R += &B[i].D + &rho[i]*&B[i].E;
-	}
+        let mut R = Point::zero();
+        for i in 0..B.len() {
+            R += &B[i].D + &rho[i] * &B[i].E;
+        }
 
-	let mut z = Scalar::zero();
-	for (i,party) in parties.iter().enumerate() {
-	    let l = Party::lambda(&party.id, parties);
-	    z += party.sign(&X, &rho[i], &R, &msg, &l);
-	}
+        let mut z = Scalar::zero();
+        for (i, party) in parties.iter().enumerate() {
+            let l = Party::lambda(&party.id, parties);
+            z += party.sign(&X, &rho[i], &R, &msg, &l);
+        }
 
-	Self {
-	    R: R,
-	    z: z,
-	}
+        Self { R: R, z: z }
     }
 
     // verify: R' = z * G + -c * X, pass if R' == R
     #[allow(non_snake_case)]
     pub fn verify(&self, X: &Point, msg: &String) -> bool {
-	let mut hasher = Sha3_256::new();
+        let mut hasher = Sha3_256::new();
 
-	hasher.update(X.compress().as_bytes());
-	hasher.update(self.R.compress().as_bytes());
-	hasher.update(msg.as_bytes());
+        hasher.update(X.compress().as_bytes());
+        hasher.update(self.R.compress().as_bytes());
+        hasher.update(msg.as_bytes());
 
-	let c = hash_to_scalar(&mut hasher);
-	let R = &self.z * G + (-c) * X;
+        let c = hash_to_scalar(&mut hasher);
+        let R = &self.z * G + (-c) * X;
 
-	println!("Verification R = {}", R);
-	
-	R == self.R
+        println!("Verification R = {}", R);
+        R == self.R
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-use secp256k1_math::{
-    point::{
-	Point,
-    },
-    scalar::Scalar,
-};
 use num_traits::identities::Zero;
 use polynomial::Polynomial;
-use rand_core::{
-    OsRng, RngCore,
-};
+use rand_core::{OsRng, RngCore};
+use secp256k1_math::{point::Point, scalar::Scalar};
 use std::env;
 
 mod frost;
@@ -16,17 +9,15 @@ mod schnorr;
 mod util;
 mod vss;
 
-use frost::{
-    Party, Share, Share2, Signature,
-};
+use frost::{Party, Share, Share2, Signature};
 
 fn eval(p: &Polynomial<Point>, x: &Scalar) -> Point {
     let mut y = x.clone();
     let mut val = p.data()[0].clone();
 
     for i in 1..p.data().len() {
-	val += &p.data()[i] * &y;
-	y *= y;
+        val += &p.data()[i] * &y;
+        y *= y;
     }
 
     val
@@ -39,77 +30,78 @@ fn main() {
     const N: usize = 3;
     const T: usize = 2;
 
-    let mut parties: Vec<Party> = (0..N).map(|n| Party::new(&Scalar::from((n+1) as u32), T, &mut rng)).collect();
+    let mut parties: Vec<Party> = (0..N)
+        .map(|n| Party::new(&Scalar::from((n + 1) as u32), T, &mut rng))
+        .collect();
     let shares: Vec<Share> = parties.iter().map(|p| p.share(&mut rng)).collect();
 
     // everybody checks everybody's shares
     for share in &shares {
-	assert!(share.verify());
+        assert!(share.verify());
     }
-    
+
     let mut agg_params = Vec::new();
     for i in 0..T {
         let mut agg = Point::default();
         for share in &shares {
-            agg +=  share.A[i];
+            agg += share.A[i];
         }
         agg_params.push(agg);
     }
     let P: Polynomial<Point> = Polynomial::new(agg_params);
 
     let zero = eval(&P, &Scalar::zero());
-    
+
     //let p = Polynomial::<Scalar>::lagrange(&xs, &ys).unwrap();
     println!("P(0) = {}", zero);
 
     // compute aggregate public key X
-    
+
     let mut X = Point::zero();
     for share in &shares {
-	X = X + &share.A[0];
+        X = X + &share.A[0];
     }
 
     println!("Aggregate public key X = {}", X);
 
     // round2
     for i in 0..N {
-	let party = parties[i].clone();
-	for j in 0..N {
-	    let party2 = &mut parties[j];
-	    
-	    // party sends party2 the round2 share
-	    party2.send(Share2{
-		i: party2.id,
-		f_i: party.f.eval(party2.id),
-	    });
-	}
+        let party = parties[i].clone();
+        for j in 0..N {
+            let party2 = &mut parties[j];
+
+            // party sends party2 the round2 share
+            party2.send(Share2 {
+                i: party2.id,
+                f_i: party.f.eval(party2.id),
+            });
+        }
     }
 
     for party in &mut parties {
-	party.compute_secret();
-	println!("Party {} secret {}", &party.id, &party.secret);
+        party.compute_secret();
+        println!("Party {} secret {}", &party.id, &party.secret);
     }
 
     // choose a random list of T parties to sign
     let mut available_parties = parties;
     let mut signing_parties = Vec::new();
     while signing_parties.len() < T {
-	let i = rng.next_u64() as usize % available_parties.len();
-	signing_parties.push(available_parties[i].clone());
-	available_parties.remove(i);
-	
+        let i = rng.next_u64() as usize % available_parties.len();
+        signing_parties.push(available_parties[i].clone());
+        available_parties.remove(i);
     }
 
     let msg = "It was many and many a year ago".to_string();
-    
+
     let S: Vec<Scalar> = signing_parties.iter().map(|p| p.id).collect();
     let mut signers = "".to_string();
     for s in S {
-	signers += &format!("{} ", s);
+        signers += &format!("{} ", s);
     }
-    
+
     println!("Signing parties {}", signers);
-    
+
     //let B: Vec<PublicNonce> = signing_parties.iter().map(|p:&mut Party| p.pop_nonce(&mut rng)).collect();
 
     let sig = Signature::new(&X, &msg, &mut signing_parties, &mut rng);
